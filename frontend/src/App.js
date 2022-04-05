d11d55b4e152e5d5ae0dd525878364465ea72033
user name <email@domain.com>
2022-04-05 11:47:29 +0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import { Route } from 'react-router-dom';
 import Header from "./components/header/index";
 import Main from "./components/main/index"
 import Navigation from "./components/navigation/index";
 import Footer from "./components/footer/index";
-import "./App.css"
+import "./App.css";
+var axios = require('axios');
 
 
 
 
 
 const App = () => {
+
+	
+
+  
+
+
+	useEffect(() => {
+		var config = {
+			method: 'get',
+			url: 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Amman&types=establishment&location=37.76999%2C-122.44696&radius=500&key=AIzaSyCyObkvRpICwcyTi-dla_IzVFnP22gHcEk',
+			headers: { }
+			 };
+		  
+			axios(config)
+			.then(function (response) {
+			console.log(JSON.stringify(response.data.predictions));
+			})
+			.catch(function (error) {
+			console.log(error);
+			});
+	}, []);
+	
+
+
+
 	return <div className="App">
+
+		
+
+
+
+
+
 		<Header></Header>
 		<Navigation></Navigation>
 		<Main></Main>
